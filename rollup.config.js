de846f34dc9eeecab2fe7af1ea8026499509f2d1
MarceloArraes <marcelo.arraes@gmail.com>
2025-10-15 15:23:23 +1030

    fix: back to fundamentals

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,14 @@ import svelte from "rollup-plugin-svelte"
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
 import { terser } from "rollup-plugin-terser"
-// We have removed copy2
+import copy2 from "rollup-plugin-copy2" // It needs this
 import json from "@rollup/plugin-json"
 import postcss from "rollup-plugin-postcss"
 import polyfill from "rollup-plugin-polyfill-node"
+import pkg from "./package.json"
 
 const production = !process.env.ROLLUP_WATCH
-
-// We have removed the 'const name = ...' line
+const name = pkg.name // This now works because pkg.name has no "/"
 
 export default {
   input: "index.js",
@@ -21,24 +21,23 @@ export default {
   },
   plugins: [
     json(),
-    svelte({
-      emitCss: true,
-    }),
+    svelte({ emitCss: true }),
     postcss({
       extract: "bundle.css",
       sourceMap: !production,
       minimize: production,
     }),
-    resolve({
-      browser: true,
-      dedupe: ["svelte"],
-    }),
+    resolve({ browser: true, dedupe: ["svelte"] }),
     commonjs(),
     polyfill(),
     production && terser(),
-    // We have removed the entire 'copy2' section from this array
+    production &&
+      copy2({
+        assets: [
+          ["package.json", `dist/${name}-${pkg.version}.tar.gz`],
+          "schema.json",
+        ],
+      }),
   ],
-  watch: {
-    clearScreen: false,
-  },
+  watch: { clearScreen: false },
 }
